ae773936e4ad162aee03a2233811ad86ac364f72
AndSelm <and.selm+github@gmail.com>
2016-08-26 20:19:13 +0200

    FIX: node functions being called before observable was subscribed to

--- src/observableFromNodeCallback.js
+++ src/observableFromNodeCallback.js
@@ -1,16 +1,6 @@
 import Rx from 'rx';
 
 /**
- * Splits the results of a node-style-callback into elements and errors
- */
-function nodeResultSelector ( err, result ) {
-  if( err )
-    throw err;
-  else
-    return result;
-}
-
-/**
  * Creates an observable from a function that takes a node-style-callbac-function
  * as the last argument.
  *
@@ -19,5 +9,12 @@ function nodeResultSelector ( err, result ) {
  * @return {Observable}
  */
 export default function observableFromNodeCallback( f, context ) {
-  return Rx.Observable.fromCallback( f, context, nodeResultSelector );
+  // fromNodeCallback already calls the function f, even if not subscribed to
+  // we thus wrap it in a new observable
+  return ( ...args ) =>
+    Rx.Observable.create( observer => {
+      const nodeFunc = Rx.Observable.fromNodeCallback( f, context );
+      const source$ = nodeFunc( ...args )
+      source$.subscribe( observer );
+    } );
 }
